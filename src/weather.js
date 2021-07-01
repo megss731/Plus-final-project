2eaf8dcea8e40bdd703f49a33fc9be3eabc63fd5
megss731 <megansmith731@yahoo.com>
2021-07-01 09:34:52 -0400

    updated js to provide date/time/conditions

--- src/weather.js
+++ src/weather.js
@@ -10,5 +10,25 @@ function getTemp(response) {
   mainDescription.innerHTML = response.data.weather[0].main;
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = response.data.main.humidity;
+  let dayTime = document.querySelector("#day-time");
+  dayTime.innerHTML = getDayAndTime(response.data.dt * 1000);
+}
+function getDayAndTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) hours = `0${hours}`;
+  let minutes = date.getMinutes();
+  if (minutes < 10) minutes = `0${minutes}`;
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}, ${hours}:${minutes}`;
 }
 axios.get(apiUrl).then(getTemp);
